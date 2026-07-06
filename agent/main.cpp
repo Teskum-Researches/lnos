6f6ccfd2e3c733cf1bdbf09bc0ad9874fa2037e8
Ruslan Sokolov <gervatyy@gmail.com>
2026-07-06 16:49:15 +0300

    [0.1] Added mutex

--- agent/main.cpp
+++ agent/main.cpp
@@ -4,9 +4,12 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <mutex>
 #include "ip.h"
-#include "../shared/protocol.h"
+#include "protocol.h"
 #include "main.h"
+
+#include "registry.h"
 
 std::string getName() {
     std::string name;
@@ -23,6 +26,7 @@ std::string getName() {
     return name;
 }
 
+std::mutex nodesMutex;
 
 std::string myName = getName();
 std::string myIp = getip();
@@ -30,26 +34,119 @@ std::string myIp = getip();
 void sender() {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
 
-    sockaddr_in server{};
-    server.sin_family = AF_INET;
-    server.sin_port = htons(PORT);
-    inet_pton(AF_INET, SERVER_IP, &server.sin_addr);
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+
+    inet_pton(AF_INET, MCAST_GROUP, &addr.sin_addr);
 
     while (true) {
-        lnos::Packet p{myName, myIp};
+        lnos::Packet p{myName};
+
         std::string msg = lnos::encode(p);
 
         sendto(sock,
                msg.c_str(),
                msg.size(),
                0,
-               reinterpret_cast<sockaddr *>(&server),
-               sizeof(server));
+               reinterpret_cast<sockaddr *>(&addr),
+               sizeof(addr));
 
         std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 }
 
+void receiver() {
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    int reuse = 1;
+
+    setsockopt(sock,
+               SOL_SOCKET,
+               SO_REUSEADDR,
+               &reuse,
+               sizeof(reuse));
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(sock, (sockaddr*)&addr, sizeof(addr));
+
+    ip_mreq mreq{};
+
+    inet_pton(AF_INET,
+              MCAST_GROUP,
+              &mreq.imr_multiaddr);
+
+    mreq.imr_interface.s_addr = INADDR_ANY;
+
+    setsockopt(sock,
+               IPPROTO_IP,
+               IP_ADD_MEMBERSHIP,
+               &mreq,
+               sizeof(mreq));
+
+    char buffer[1024];
+
+    while (true) {
+        sockaddr_in senderAddr{};
+        socklen_t senderLen = sizeof(senderAddr);
+
+        int len = recvfrom(sock,
+                           buffer,
+                           sizeof(buffer) - 1,
+                           0,
+                           reinterpret_cast<sockaddr *>(&senderAddr),
+                           &senderLen);
+
+        if (len <= 0)
+            continue;
+
+        buffer[len] = 0;
+
+        char ip[INET_ADDRSTRLEN];
+
+        inet_ntop(AF_INET,
+                  &senderAddr.sin_addr,
+                  ip,
+                  sizeof(ip));
+
+        lnos::Packet p = lnos::decode(buffer);
+
+        std::lock_guard<std::mutex> lock(nodesMutex);
+
+        nodes[p.name] = {
+            p.name,
+            ip,
+            std::chrono::steady_clock::now()
+        };
+
+        std::lock_guard<std::mutex> unlock(nodesMutex);
+    }
+}
+
+void printer() {
+    while (true) {
+        std::cout << "\033[2J\033[H";
+        std::cout << "=== LNOS NODES ===" << std::endl;
+        std::lock_guard<std::mutex> lock(nodesMutex);
+        for (const auto& n : nodes) {
+            std::cout << n.second.name
+            << " - " << n.second.ip << std::endl;
+        }
+        std::lock_guard<std::mutex> unlock(nodesMutex);
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+}
+
 int main() {
-    sender();
+    std::thread t1(sender);
+    std::thread t2(receiver);
+    std::thread t3(printer);
+
+    t1.join();
+    t2.join();
+    t3.join();
 }
--- agent/main.h
+++ agent/main.h
@@ -2,6 +2,8 @@
 
 
 #define SERVER_IP "127.0.0.1"
+
+#define MCAST_GROUP "239.255.42.99"
 #define PORT 4545
 
 std::string getName();
