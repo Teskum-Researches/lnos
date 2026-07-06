12b98e1fb7a95788b1990f1f6ab2a8000a685928
new-user12345678 <you@example.com>
2026-07-06 13:19:07 +0300

    Real ip

---
+++ agent/ip.cpp
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+std::string getip()
+{
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    strcpy(ifr.ifr_name, "eth0");
+
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+    ioctl(s, SIOCGIFADDR, &ifr);
+    close(s);
+
+    struct sockaddr_in *sa = (struct sockaddr_in*)&ifr.ifr_addr;
+    std::string ipadress = inet_ntoa(sa->sin_addr);
+    // printf("addr = %s\n", inet_ntoa(sa->sin_addr));
+    return ipadress;
+}
+
