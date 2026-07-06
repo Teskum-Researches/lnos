3e097bc30e31402f8140cde075c4ec9ed6d0aa6c
Ruslan Sokolov <gervatyy@gmail.com>
2026-07-06 12:16:56 +0200

    Name from file

--- agent/main.cpp
+++ agent/main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <thread>
 #include <arpa/inet.h>
@@ -9,8 +10,24 @@
 #define SERVER_IP "127.0.0.1"
 #define PORT 4545
 
-std::string myName = "pc.main";
-std::string myIp = "192.168.0.10";
+std::string getName() {
+    std::string name;
+    std::ifstream nameFile("/etc/lnos/name");
+
+    if (!nameFile.is_open()) {
+        std::cerr << "lnos: Error: Unable to open the file" << std::endl;
+        return name;
+    }
+    nameFile >> name;
+
+    nameFile.close();
+
+    return name;
+}
+
+
+std::string myName = getName();
+std::string myIp = "0.0.0.0";
 
 void sender() {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -28,7 +45,7 @@ void sender() {
                msg.c_str(),
                msg.size(),
                0,
-               (sockaddr*)&server,
+               reinterpret_cast<sockaddr *>(&server),
                sizeof(server));
 
         std::this_thread::sleep_for(std::chrono::seconds(2));
