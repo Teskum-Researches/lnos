bb1167676f090e88102dd90d594c844a598d9970
oxxide216 <oxxide216@gmail.com>
2026-07-16 17:07:46 +0300

    Added better error reporting to lnosctl

--- lnosctl/src/main.cpp
+++ lnosctl/src/main.cpp
@@ -33,10 +33,10 @@ bool writeKey(const char* path, const unsigned char* key, std::size_t size)
 
 bool generateKeys() {
     if (sodium_init() < 0) {
-        std::cout << "Failed to initialize libsodium!" << std::endl;
+        std::cerr << "Failed to initialize libsodium!" << std::endl;
         return false;
     } if (geteuid() != 0) {
-        std::cout << "Must be run as root" << std::endl;
+        std::cerr << "Must be run as root" << std::endl;
         return false;
     }
 
@@ -56,29 +56,37 @@ bool generateKeys() {
     return true;
 }
 
+void printUsage(char *program_name) {
+    std::cerr << "Usage: " << program_name << " <command>\n";
+    std::cerr << "Available commands:\n";
+    std::cerr << "    generatekeys          generate private and public keys for LNOS node\n";
+    std::cerr << "    init                  create LNOS config\n";
+    std::cerr << "    config                print LNOS config\n";
+    std::cerr << "    set                   override LNOS config property\n";
+    std::cerr << "    get                   print LNOS config property\n";
+}
+
 int main(int argc, char** argv)
 {
     auto cfg = lnos::loadConfig();
 
     if (argc < 2)
     {
-        std::cout << "Usage: lnosctl <command>\n";
+        printUsage(argv[0]);
         return 1;
     }
 
-    if (argv[1] == "generatekeys") {
+    std::string command = argv[1];
+    if (command == "generatekeys") {
         if (geteuid() == 0) {
             generateKeys();
         } else {
-            std::cout << "Must be run as root" << std::endl;
+            std::cerr << "Must be run as root" << std::endl;
             return -1;
         }
-    }
-    std::string command = argv[1];
-
-    if (command == "init") {
+    } else if (command == "init") {
         if (geteuid() != 0) {
-            std::cout << "Must be run as root" << std::endl;
+            std::cerr << "Must be run as root" << std::endl;
             return 1;
         }
 
@@ -90,10 +98,10 @@ int main(int argc, char** argv)
         return 0;
     } else if (command == "set") {
         if (geteuid() != 0) {
-            std::cout << "Must be run as root" << std::endl;
+            std::cerr << "Must be run as root" << std::endl;
             return 1;
         }
-        if (argv[2] == nullptr || argv[3] == nullptr) {
+        if (argc < 4) {
             std::cerr << "Not enough arguments" << std::endl;
             return 1;
         }
@@ -105,7 +113,7 @@ int main(int argc, char** argv)
 
         return 0;
     } else if (command == "get") {
-        if (argv[2] == nullptr) {
+        if (argc < 3) {
             std::cerr << "Not enough arguments" << std::endl;
             return 1;
         }
@@ -116,5 +124,8 @@ int main(int argc, char** argv)
             std::cout << "Node Name: " << cfg.name << std::endl;
         }
         return 0;
+    } else {
+        printUsage(argv[0]);
+        std::cerr << "Unknown subcommand " << command << std::endl;
     }
 }
